d9977bc7e4b80439df1f4755c2504bd59789c0e1
qtgeo1248 <gzhou7819@gmail.com>
2020-10-04 14:55:42 -0400

    fixed tabbing and formatting

--- website/example_problem_graders/optimization.py
+++ website/example_problem_graders/optimization.py
@@ -18,6 +18,7 @@ Expects initial grader_data to be formatted as
 class OptGrader(BaseGrader):
     def __init__(self, problem):
         self.problem = problem
+        self.n = int(problem.grader_data["n"])
 
     def grade(self, submission):
         '''
@@ -36,32 +37,34 @@ class OptGrader(BaseGrader):
                 return False
             for part in parts:
                 try: 
-                    int(user_input)
+                    int(part)
                 except ValueError:
                     return False
             if (1 > parts[0]) or (parts[0] >= parts[1]) or (parts[1] > self.n):
                 return False
-        return true
+        return True
 
     # Assumes the validity of user_input
     def parse(self, user_input):
+        '''
+        Returns an array of 2-element arrays
+        '''
         edges = []
         lines = user_input.split("\n")
         for line in lines:
             parts = line.split()
             edges.append(parts)
         return edges
-
-
-    def calc(self, nodes, edges):
-	'''
-	Calculates the number of triangles in a given graph
-	'''
-	EdgeSet, ans = set(), 0
-	for x in edges:
-	    EdgeSet.add(nodes * (x[0] - 1) + x[1] - 1)
-	for x in edges:
-	    for y in edges:
-		if x[1] == y[0] and nodes * (x[0] - 1) + y[1] - 1 in EdgeSet:
-		    ans += 1
-	return ans
+    
+    def calc(self, edges):
+        '''
+        Calculates the number of triangles in a given graph
+        '''
+        EdgeSet, ans = set(), 0
+        for x in edges:
+            EdgeSet.add(self.n * (x[0] - 1) + x[1] - 1)
+        for x in edges:
+            for y in edges: 
+                if x[1] == y[0] and self.n * (x[0] - 1) + y[1] - 1 in EdgeSet:
+                    ans += 1
+        return ans
