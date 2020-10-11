6ba79deb559517c366f0eebc95eb541c890acdb2
qtgeo1248 <gzhou7819@gmail.com>
2020-10-11 15:10:11 -0400

    made a team name

--- website/models.py
+++ website/models.py
@@ -185,7 +185,7 @@ class Team(models.Model):
             registered team are finalized and cannot be edited'))
     team_leader = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, \
             on_delete=models.SET_NULL)
-    team_name = models.CharField(max_length=100,unique=true)
+    team_name = models.CharField(max_length=100, unique=True)
     invite_code = models.IntegerField()
 
     def __str__(self):
