5017a32d5afef12342bfd750b8085cb5b6f82b59
David Tang <itangdav@gmail.com>
2020-12-28 11:58:18 -0500

    added rank display to all problems page

--- website/views/exam.py
+++ website/views/exam.py
@@ -15,9 +15,21 @@ def all_problems(request, exam_id):
 
         scores = []
         task_scores = []
+
+        # Calculates the number of people with higher score + 1
+        ranks = []
+        total_nums = []
+
         for problem in problems:
             curr_score = Score.objects.getScore(problem, competitor)
             scores.append(curr_score)
+
+            all_scores = Score.objects.filter(problem=problem)
+            problem_rank = 1
+            for score in all_scores:
+                if score.points > curr_score.points and score.problem_id == curr_score.problem_id:
+                    problem_rank+=1
+
             if exam.is_optimization:
                 temp = []
                 for i in range(len(curr_score.task_scores)):
@@ -25,18 +37,25 @@ def all_problems(request, exam_id):
                 task_scores.append(temp)
             else:
                 task_scores.append(None)
+
+            ranks.append(problem_rank)
+            total_nums.append(len(all_scores))
+
     else:
         scores = []
         task_scores = []
+        ranks = []
+        total_nums = []
         for problem in problems:
             scores.append(None)
             task_scores.append(None)
-
+            ranks.append(None)
+            total_nums.append(None)
 
 
     context = {
         'exam': exam,
-        'all_problems_scores': zip(problems, scores, task_scores),
+        'all_problems_scores_ranks': zip(problems, scores, task_scores,ranks, total_nums),
     }
     return render(request, 'exam/all_problems.html', context)
 
